ced0c964482ba1b7bdefef3c83ff455be8e73080
Lynnxanax <zeec1970@gmail.com>
2026-03-16 20:32:17 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+function abrirCarta(){
+
+document.getElementById("envelope").style.display="none";
+document.getElementById("carta").classList.remove("hidden");
+
+let musica = document.getElementById("musica");
+musica.play();
+
+}
